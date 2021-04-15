f8927e28d7e83a3367248069a9e8eb51b8844885
rajaram95 <rajaramml8148@gmail.com>
2021-04-15 12:53:37 +0530

    Updated whatsup

---
+++ src/test/java/org/sample/Github.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Github {
+		private void testA() {
+System.out.println("TestA=====");
+
+		}
+private void testB() {
+System.out.println("TestB=====");
+}		
+	}
+
+
